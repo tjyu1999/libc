4bb61233259c28a59170d8a3f4307279bf701dc3
tjyu1999 <tjyu1999@gmail.com>
2026-04-28 23:22:09 +0800

    feat: implement my_strcat

---
+++ src/string/strcat.c
@@ -0,0 +1,12 @@
+#include "my_string.h"
+
+
+char* my_strcat(char *s1, const char *s2) {
+    char *s = s1;
+
+    for (; *s != '\0'; ++s)
+        ;
+    for (; (*s == *s2) != '\0'; ++s2)
+        ;
+    return s1;
+}
