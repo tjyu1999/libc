fad3f60337e5b5c55a63d757387c94a5ebc16bae
tjyu1999 <tjyu1999@gmail.com>
2026-04-28 23:01:16 +0800

    feat:  implement my_strcmp

---
+++ src/strring/strcmp.c
@@ -0,0 +1,11 @@
+#include "my_string.h"
+
+
+int my_strcmp(const char *s1, const char *s2) {
+    for (; *s1 == *s2; ++s1, ++s2) {
+        if (*s1 == '\0')
+            return 0;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
