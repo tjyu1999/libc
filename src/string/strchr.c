d6fd6cb9ca1edfb9108ab189e3a652de68a10938
tjyu1999 <tjyu1999@gmail.com>
2026-06-09 22:55:13 +0800

    feat: implement my_strchr

---
+++ src/string/strchr.c
@@ -0,0 +1,12 @@
+#include "my_string.h"
+
+
+char* my_strchr(const char *s, int c) {
+    const char ch = c;
+
+    for (; *s != ch; ++s) {
+        if (*s == '\0')
+            return NULL;
+    }
+    return (char*)s;
+}
